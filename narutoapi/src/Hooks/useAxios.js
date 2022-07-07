a73618b18b4a1ce831c945b7324aafe65516a279
LucasG-Nunes <lucasgnam12@gmail.com>
2022-07-07 15:25:37 -0300

    :hammer: Estruturando as requisições HTTP para os produtos e CEP

--- narutoapi/src/Hooks/useAxios.js
+++ narutoapi/src/Hooks/useAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
-import {useEffect, useState} from "react"
-
-const useAxios = () => {
+import {useEffect, useState, useContext} from "react"
+import { ContextInput } from "../Context/ContextInput"
+export const useAxios = () => {
     const [products,setProducts] = useState()
     async function requestApi() {
       const response = await axios.get("http://dummyjson.com/products")
@@ -16,4 +16,20 @@ const useAxios = () => {
   return {products}
 }
 
-export default useAxios
+export const useAxiosCEP = ()=>{
+  const [cep,setCEP] = useState()
+  const {input} = useContext(ContextInput)
+  if(!input){
+    console.log('Está vazio')
+  }
+  async function requestCEP(){
+    const response = await axios.get(`https://viacep.com.br/ws/${input}/json`)  
+    setCEP(response.data)
+  }
+  useEffect(()=>{
+    requestCEP()
+    })
+
+    return {cep}
+}
+
