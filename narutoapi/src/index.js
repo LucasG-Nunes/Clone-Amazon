e98838cd21f3f098a1a498d9c11b3272991b2a1e
LucasG-Nunes <lucasgnam12@gmail.com>
2022-07-07 15:28:34 -0300

    :rocket: Adicionando ContextAPI do CEP para reutiliza-lo globalmente no projeto

--- narutoapi/src/index.js
+++ narutoapi/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ContextInputProvider } from "./Context/ContextInput";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ContextInputProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ContextInputProvider>
   </React.StrictMode>
 );
 
