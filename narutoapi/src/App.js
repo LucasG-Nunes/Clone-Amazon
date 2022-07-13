698fc3ca92a587557b1a5243399d53bd94eda534
LucasG-Nunes <lucasgnam12@gmail.com>
2022-07-13 11:01:34 -0300

    :rocket: Construindo rotas da página de carrinhos e login

--- narutoapi/src/App.js
+++ narutoapi/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Shoppingcart from "./Pages/Shoppingcart";
 //usar grid
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/shoppingcart" element={<Shoppingcart />} />
         </Routes>
       </BrowserRouter>
     </div>
